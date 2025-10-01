dbfef130be1d7c8082557f65ac1df376dda0cb5d
floyddc <diegocoli68@gmail.com>
2025-10-01 13:14:57 +0200

    renamed main

---
+++ src/main/java/pcd/ass03/Main.java
@@ -0,0 +1,61 @@
+/*
+    COMPONENTI DEL GRUPPO:
+    Arcese Gabriele
+    Colì Diego
+    Costantini Marco
+    Meco Daniel
+ */
+
+package pcd.ass03;
+
+import pcd.ass03.actors.SimulationController;
+import pcd.ass03.model.BoidModel;
+import pcd.ass03.view.BoidsView;
+import javax.swing.JOptionPane;
+
+public class Main {
+    
+    final static double SEPARATION_WEIGHT = 1.0;
+    final static double ALIGNMENT_WEIGHT = 1.0;
+    final static double COHESION_WEIGHT = 1.0;
+
+    final static int ENVIRONMENT_WIDTH = 1000; 
+    final static int ENVIRONMENT_HEIGHT = 1000;
+    static final double MAX_SPEED = 4.0;
+    static final double PERCEPTION_RADIUS = 50.0;
+    static final double AVOID_RADIUS = 20.0;
+
+    final static int SCREEN_WIDTH = 1000; 
+    final static int SCREEN_HEIGHT = 800; 
+
+    public static void main(String[] args) {      
+        // Chiede all'utente di inserire il numero di boids
+        String input = JOptionPane.showInputDialog("Inserisci il numero di boids:");
+        int nBoids = 1500; // valore default
+        try {
+            nBoids = Integer.parseInt(input);
+        } catch(NumberFormatException e) {
+            System.out.println("Numero non valido, utilizzo valore di default 1500.");
+        }
+        
+        var model = new BoidModel(
+                        nBoids, 
+                        SEPARATION_WEIGHT, ALIGNMENT_WEIGHT, COHESION_WEIGHT, 
+                        ENVIRONMENT_WIDTH, ENVIRONMENT_HEIGHT,
+                        MAX_SPEED,
+                        PERCEPTION_RADIUS,
+                        AVOID_RADIUS); 
+        
+        // Crea il controller della simulazione
+        SimulationController controller = new SimulationController(model);
+
+        // Ottieni il simulator dal controller
+        var simulator = controller.getSimulator();
+        
+        // Avvia la simulazione
+        simulator.start();
+        
+        // Collega la GUI al controller
+        BoidsView view = new BoidsView(model, simulator, controller, SCREEN_WIDTH, SCREEN_HEIGHT);
+    }
+}
